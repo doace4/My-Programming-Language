40295b2f42f3271d80751d2639fdde5026afc9dc
doace4 <56239530+doace4@users.noreply.github.com>
2023-11-01 01:57:06 -0400

    Update Interpreter.py

--- Interpreter.py
+++ Interpreter.py
@@ -4,37 +4,59 @@ with open("program.txt", "r") as file:
 for x, y in enumerate(read):
     read[x] = y.replace("\n", "")
 
-count = 0
+memory = [0] * int(read[0].replace("memory ", ""))
+lines = dict()
+count = 1
 
 while count < len(read):
-    code = read[count]
+    instruction = []
+    inputs = read[count].split(": ")
 
-    if "print" in code:
-        result = globals()[code.split(": ")[1]]
-        print(result)
+    if "set" in read[count]:
+        instruction.append("set")
+        instruction.append(inputs[1])
+        instruction.append(inputs[2])
 
-    elif "set" in code:
-        code = code.replace("set ", "")
-        code = code.split(": ")
+    elif "send" in read[count]:
+        instruction.append("send")
+        instruction.append(inputs[1])
 
-        try:
-            globals()[code[0]] = int(code[1])
+    elif "inc" in read[count]:
+        instruction.append("inc")
+        instruction.append(inputs[1])
 
-        except ValueError:
-            globals()[code[0]] = code[1]
+    elif "dec" in read[count]:
+        instruction.append("dec")
+        instruction.append(inputs[1])
 
-    elif "inc" in code:
-        code = code.split(": ")
-        globals()[code[1]] += 1
-        
-    elif "dec" in code:
-        code = code.split(": ")
-        globals()[code[1]] -= 1
+    elif "condition" in read[count]:
+        instruction.append("condition")
+        instruction.append(inputs[1])
+        instruction.append(inputs[2])
 
-    elif "condition" in code:
-        code = code.split(": ")
+    lines[count] = instruction
+    count += 1
 
-        if eval(code[1]):
-            count += 1
+count = 1
 
+while count <= len(lines):
+    typeofinstruction = lines[count][0]
+    value = int(lines[count][1], 16)
+
+    if typeofinstruction == "set":
+        memory[value] = lines[count][2]
+
+    elif typeofinstruction == "send":
+        print(value)
+
+    elif typeofinstruction == "inc":
+        memory[value] += 1
+
+    elif typeofinstruction == "dec":
+        memory[value] -= 1
+
+    elif typeofinstruction == "condition":
+        if eval(lines[count][1]):
+            count = int(lines[count][2])
+    
     count += 1
